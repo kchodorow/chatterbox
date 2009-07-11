22c77e9a895fc1db677bb16f7e24b2dffaeabbe6
Kristina <kristina@10gen.com>
2009-07-11 16:51:00 -0400

    click to load comments

--- chrome/content/vote.js
+++ chrome/content/vote.js
@@ -1,8 +1,30 @@
+var serverURL = "http://www.snailinaturtleneck.com/chatterbox/util.php";
+
+function getHref() {
+    return escape(window.content.document.location);
+}
+
+
 function vote(pm) {
-    $.post("http://www.snailinaturtleneck.com/chatterbox",
-           {"vote" : (pm ? 1 : 0)},
+    $.post(serverURL,
+           {
+	       "vote" : (pm ? 1 : 0),
+	       "page" : getHref()
+	   },
            function(data){
                alert(data.msg);
            },
            "json");
+}
+
+function loadComments() {
+    $.get(serverURL,
+	  {"comments" : getHref()},
+	  function(data) {
+	      alert("hi");
+	  });
+}
+
+function postComment() {
+    return;
 }
